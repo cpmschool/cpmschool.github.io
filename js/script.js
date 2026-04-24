c1408b3f9286a65ee3b4a25a2703739e36b089fb
cpmschool <romail.mtajj16@gmail.com>
2026-04-24 21:01:04 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ function openImage(pic) {
 function closeImage() {
   document.getElementById("imageBox").style.display = "none";
 }
+function openImage(pic) {
+  document.getElementById("imageBox").style.display = "flex";
+  document.getElementById("fullImage").src = pic.src;
+}
+
+function closeImage() {
+  document.getElementById("imageBox").style.display = "none";
+}
