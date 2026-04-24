afa16ef759b7c32d9119f0369b3f87ec7e0823ee
cpmschool <romail.mtajj16@gmail.com>
2026-04-24 21:18:27 +0400

    Implement slide navigation and auto slide functionality
    Added functions to handle slide navigation and auto slide feature.

--- js/script.js
+++ js/script.js
@@ -14,3 +14,28 @@ function openImage(pic) {
 function closeImage() {
   document.getElementById("imageBox").style.display = "none";
 }
+let index = 0;
+
+function showSlide(i) {
+  const slides = document.getElementById("slides");
+  const total = slides.children.length;
+
+  if (i >= total) index = 0;
+  else if (i < 0) index = total - 1;
+  else index = i;
+
+  slides.style.transform = "translateX(" + (-index * 100) + "%)";
+}
+
+function nextSlide() {
+  showSlide(index + 1);
+}
+
+function prevSlide() {
+  showSlide(index - 1);
+}
+
+/* AUTO SLIDE */
+setInterval(() => {
+  nextSlide();
+}, 4000);
