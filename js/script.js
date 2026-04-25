e1812c8084ea30fd0f1fbe735474a82dd26ad743
cpmschool <romail.mtajj16@gmail.com>
2026-04-25 08:08:02 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-function openImage(pic) {
-  document.getElementById("imageBox").style.display = "flex";
-  document.getElementById("fullImage").src = pic.src;
+// MENU
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("show");
 }
 
-function closeImage() {
-  document.getElementById("imageBox").style.display = "none";
-}
-function openImage(pic) {
-  document.getElementById("imageBox").style.display = "flex";
-  document.getElementById("fullImage").src = pic.src;
-}
-
-function closeImage() {
-  document.getElementById("imageBox").style.display = "none";
-}
+// SLIDER
 let index = 0;
 
 function showSlide(i) {
@@ -24,7 +14,7 @@ function showSlide(i) {
   else if (i < 0) index = total - 1;
   else index = i;
 
-  slides.style.transform = "translateX(" + (-index * 100) + "%)";
+  slides.style.transform = "translateX(-" + (index * 100) + "%)";
 }
 
 function nextSlide() {
@@ -35,44 +25,4 @@ function prevSlide() {
   showSlide(index - 1);
 }
 
-/* AUTO SLIDE */
-setInterval(() => {
-  nextSlide();
-}, 4000);show");
-    }
-const elements = document.querySelectorAll(".animate");
-
-window.addEventListener("scroll", () => {
-  const trigger = window.innerHeight * 0.85;
-
-  elements.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-
-    if (top < trigger) {
-      el.classList.add("show");
-    }
-  });
-});
-
-let currentIndex = 0;
-let images = document.querySelectorAll(".grid img");
-
-function openImage(pic) {
-  document.getElementById("imageBox").style.display = "flex";
-  document.getElementById("fullImage").src = pic.src;
-  currentIndex = Array.from(images).indexOf(pic);
-}
-
-function closeImage() {
-  document.getElementById("imageBox").style.display = "none";
-}
-
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  document.getElementById("fullImage").src = images[currentIndex].src;
-}
-
-function prevImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  document.getElementById("fullImage").src = images[currentIndex].src;
-}
+setInterval(nextSlide, 4000);
